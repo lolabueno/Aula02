770bffb11455bfbbf0094a2de1a56bcf74f3c897
Caroline <carolinebueno2005@gmail.com>
2021-07-28 21:09:52 -0300

    Implementado o if encadeado

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -13,7 +13,17 @@ public class DesvioEncadeado {
 		idade = leitor.nextInt();
 	
 		/*Aqui devemos criar uma solução para realizar as verificações.*/
-		
+		if (idade<16) {
+			System.out.println("O passageiro nao pode embarcar");
+		}
+		else {
+			if (idade>=18) {
+				System.out.println("O passageiro e obrigado a votar e pode embarcar");
+			}
+			else {
+				System.out.println("O passageiro pode votar e pode embarcar");
+			}
+		}
 		
 	
 		leitor.close();
